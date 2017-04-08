ea59cd0ec94d0bb27702d1d860f8af2a7764b102
gnorambuena <gnorambuena@users.noreply.github.com>
2017-04-08 16:37:49 -0300

    Added Activity wrapper

---
+++ activity.js
@@ -0,0 +1,39 @@
+/**
+ * Created by gabriel on 08-04-17.
+ */
+'use strict';
+
+class Activity{
+
+    constructor(name,limit,notifyUsers){
+        this.name = name;
+        this.limit = limit;
+        this.users = [];
+        this.notifyUsers = notifyUsers;
+        this.time = 0;
+        this.place = '';
+    }
+
+    setTime(time){
+        this.time = time;
+    }
+
+    setPlace(place){
+        this.place = place;
+    }
+
+    addMember(username){
+        if(users.length<limit){
+            users.push(username);
+        }
+        else{
+            this.notifyUsers(this.time,this.place);
+        }
+    }
+
+    getMembers(){
+        return this.users;
+    }
+}
+
+module.exports = Activity;
