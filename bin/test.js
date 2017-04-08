9f4870694b506643c5faa905656dd86932dfc4b5
Javier <jv1855@hotmail.com>
2017-04-08 19:56:22 -0300

    Aqui teni tu commit message ctm

--- bin/test.js
+++ bin/test.js
@@ -4,11 +4,24 @@ const login = require("facebook-chat-api");
 login({email: "freetime.bot@gmail.com", password : "todossomosbruno123"}, (err, api) => {
    if(err) return console.error(err);
 
-   api.sendMessage("Wena",1076235082);
+    let friends = [];
 
-   api.listen((err, message) => {
-       console.log(message.body);
-       api.sendMessage(message.body,message.threadID);
+   api.getFriendsList((err, data) =>
+   {
+       for(friend of data)
+       {
+           friends.push(friend.userID.toString());
+           console.log(friend.userID);
+       }
+
+       api.sendMessage("HOLA QUIEREN CULEAR O ALGO",friends);
    });
+
+    api.listen((err,message) =>
+    {
+        api.sendMessage(message.body,message.threadID);
+        console.log(message.threadID);
+    });
+
 });
 
